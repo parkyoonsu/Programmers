cf632fe626a5aa12c26c59a784fb1071173c7aa0
Yunsu_Park <126734281+parkyoonsu@users.noreply.github.com>
2026-05-27 22:53:07 +0900

    Add files via upload

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,10 @@
 import mul, { add, sub } from "./math.js"; //동일한 경로로 불러올 때는 ","로 합쳐서 불러올 수 있음.
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+import randomcolor from "randomcolor";
+
+const color = randomcolor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
