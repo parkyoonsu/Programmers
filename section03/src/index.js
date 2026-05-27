c405097bf05e5cb105b6242a7fa0d5fafba6c1ed
Yunsu_Park <126734281+parkyoonsu@users.noreply.github.com>
2026-05-27 22:43:25 +0900

    Add files via upload

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,5 @@
-console.log("안녕 Node.js");
+import mul, { add, sub } from "./math.js"; //동일한 경로로 불러올 때는 ","로 합쳐서 불러올 수 있음.
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
---
+++ section03/src/math.js
@@ -0,0 +1,13 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) { //대표하는 단 하나의 method
+  return a * b;
+}
